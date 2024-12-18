4b2ed4ad566b0802cd70f603392f82b986cbbd8e
ranjit1024 <ranjitdas2048@gmail.com>
2024-12-18 09:34:04 +0530

    adding dragging behevoior

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 let newX = 0, newY = 0, startX = 0, startY = 0;
 const card = document.querySelector(".card");
 
-card.addEventListener("mousedown",(e)=>{
-    console.log('ioj')
-})
+card.addEventListener("mousedown",mouseDown);
+
+function mouseDown(e){
+    startX = e.clientX;
+    startY = e.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+}
+
+function mouseMove(e){
+    newX =  startX - e.clientX;
+    newY = startY - e.clientY;
+
+    startX = e.clientX;
+    startY = e.clientY;
+
+    card.style.top = (card.offsetTop-newY)+ "px";
+    card.style.left = (card.offsetLeft - newX) + "px"
+
+    console.log(newX,newY)
+}
+function mouseUp(e){
+    document.removeEventListener("mousemove", mouseMove)
+}
